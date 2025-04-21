4e1b0d4750b8a7fb517bdd759f113f70c4c878c6
Joshua Matte <joshua@bloomfieldhomes.net>
2025-04-21 12:27:36 -0500

    Remove unused sum function and correct multiply function implementation

--- script.js
+++ script.js
@@ -41,20 +41,8 @@ const subtract = function(a, b) {
 	return ( a - b)
 };
 
-const sum = function(arr) {
-	let sum = 0
-  for (let i = 0; i < arr.length; i++) {
-    sum += arr[i];
-  }
-  return sum
-};
-
-const multiply = function(arr) {
-  let product = 1;
-  for (let i = 0; i < arr.length; i++) {
-    product *= arr[i];
-  }
-  return product;
+const multiply = function(a, b) {
+    return ( a - b)
 };
 
 document.addEventListener('keydown', keyPress);
