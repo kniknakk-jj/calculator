e6395ac671e3a2a35e18260d5a94018b9c68121c
Joshua Matte <joshua@bloomfieldhomes.net>
2025-04-21 12:05:13 -0500

    Refactor HTML structure and enhance JavaScript functionality for calculator

--- script.js
+++ script.js
@@ -2,6 +2,27 @@ const display = document.getElementById('display')
 
 function appendToDisplay(input) {
     display.value += input
+    console.log(input)
+}
+
+function keyPress(event) {
+    const allowedKeys = ['0','1','2','3','4','5','6','7','8','9','+','-','*','/','.','Escape'];
+    
+    if (event.key === "Escape") {
+        return clearDisplay();
+    } else if (allowedKeys.includes(event.key)) {
+        appendToDisplay(event.key);
+    } else if (event.key === 'Enter') {
+        try {
+            display.value = eval(display.value);
+        } catch {
+            display.value = "Error";
+        }
+    } else if (event.key === 'Backspace') {
+        display.value = display.value.slice(0, -1);
+    }
+
+    event.preventDefault();
 }
 
 function clearDisplay() {
@@ -11,3 +32,5 @@ function clearDisplay() {
 function calculate() {
     display.value
 }
+
+document.addEventListener('keydown', keyPress);
